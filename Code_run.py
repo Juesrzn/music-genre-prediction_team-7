651c8387c8aabc1128f222ac73b2ec19275bd2ad
Juesrzn <164930920+Juesrzn@users.noreply.github.com>
2024-03-25 23:55:09 +0300

    Update Code_run.py

--- Code_run.py
+++ Code_run.py
@@ -5,5 +5,5 @@ TRAIN = "https://www.dropbox.com/scl/fi/5zy935lqpaqr9lat76ung/music_genre_train.
 TEST = "https://www.dropbox.com/scl/fi/o6mvsowpp9r3k2lejuegt/music_genre_test.csv?rlkey=ac14ydue0rzlh880jwj3ebum4&dl=1"
 
 df = pd.read_csv(TRAIN)
-st.title('Rock-n-Code')
+st.title('Code-team-7')
 st.write(df)
